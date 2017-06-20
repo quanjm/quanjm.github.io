842cb83ad7cf755d3f3c98b448b2728dd0f7c7c5
qjm <q_j_m@outlook.com>
2017-06-20 13:58:34 +0800

    Site updated: 2017-06-20 13:58:30

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/quanjm/blog-back/blob/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/quanjm/blog-back/blob/master/' + data.link[i];
+          var minSrc = 'https://github.com/quanjm/Blog-Back-Up/Blog-Back-Up/master/min_photos/' + data.link[i];
+          var src = 'https://github.com/quanjm/Blog-Back-Up/Blog-Back-Up/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
